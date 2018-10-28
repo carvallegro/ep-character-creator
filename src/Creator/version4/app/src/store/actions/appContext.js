d237b3c31cb1d87301a6df9bafe9191f99ca0acf
Carvallegro <valentin.carmignac@gmail.com>
2018-10-28 17:06:19 +1100

    fixed first load process

--- src/Creator/version4/app/src/store/actions/appContext.js
+++ src/Creator/version4/app/src/store/actions/appContext.js
@@ -1,10 +1,12 @@
 import dispatcher from '../../client/dispatcher'
 
 import creditsActions from './credits'
+import refDataActions from './ref-data'
 
 export const appContextTypes = {
   SESSION_CHECK: 'APP_CONTEXT_SESSION_CHECK',
-  LOADING_START: 'APP_CONTEXT_LOADING_START'
+  LOADING_START: 'APP_CONTEXT_LOADING_START',
+  LOADING_STOP: 'APP_CONTEXT_LOADING_STOP'
 }
 
 const checkExistingSession = () => ({
@@ -12,14 +14,39 @@ const checkExistingSession = () => ({
   payload: dispatcher.dispatcher('firstTime=first&getCrePoint=get')
 })
 
+const loadingStart = () => ({type: appContextTypes.LOADING_START})
+const loadingStop = () => ({type: appContextTypes.LOADING_STOP})
+
+const appLoad = () =>
+  dispatch => {
+    dispatch(loadingStart())
+    dispatch(checkExistingSession())
+      .then(({value}) => {
+        if (value.sessionExist) {
+          return dispatch(refDataActions.loadAllRefData())
+        }
+      })
+      .then(() => dispatch(loadingStop()))
+  }
+
 const startNewCharacter = (initialCredits) =>
   dispatch => {
-    dispatch({type: appContextTypes.LOADING_START})
+    dispatch(loadingStart())
     return dispatch(creditsActions.setCP(initialCredits))
       .then(() => dispatch(checkExistingSession()))
+      .then(() => dispatch(loadingStart()))
+      .then(({value}) => {
+        if (value.sessionExist) {
+          return dispatch(refDataActions.loadAllRefData())
+        }
+      })
+      .then(() => dispatch(loadingStop()))
   }
 
 export default {
   checkExistingSession,
-  startNewCharacter
+  appLoad,
+  startNewCharacter,
+  loadingStart,
+  loadingStop
 }
