be290fab1e15fea5d4be753eb7434f095ab6c5d8
Carvallegro <valentin.carmignac@gmail.com>
2018-11-03 11:41:52 +1100

    changed wrapper into component

--- src/Creator/version4/app/src/common/labelled-value/inputs.js
+++ src/Creator/version4/app/src/common/labelled-value/inputs.js
@@ -1,33 +1,52 @@
 import kebabCase from 'lodash/fp/lowerCase'
 
 import React from 'react'
-import { compose } from 'recompose'
+import { compose, withStateHandlers } from 'recompose'
 import { withTheme } from 'styled-components'
 import { Icon } from 'carbon-components-react'
 import { iconCheckmark, iconClose } from 'carbon-icons'
 
 import { InputActionsWrapper, InputButtonWrapper, InputWrapper, StyledTextInput } from './inputs.style'
 
-export const withInputWrapper = Component => props =>
+
+
+const stateHandlers = withStateHandlers(
+    props => ({
+        inputValue: props.value
+    }),
+    {
+        updateStateValue: () => value => ({
+            inputValue: value
+        })
+    }
+)
+
+export const TextInput = compose(withTheme, stateHandlers)(({ title, value, inputValue, updateStateValue, onValidate, onCancel, theme }) =>
+    <InputContainer onValidate={onValidate} onCancel={onCancel} theme={theme}>
+        <StyledTextInput
+            id={kebabCase(title)}
+            hideLabel={true}
+            value={inputValue}
+            labelText=''
+            onChange={e => updateStateValue(e.target.value)}
+            light/>
+    </InputContainer>)
+
+const InputContainer = props =>
     <InputWrapper>
-        <Component {...props} />
+        {props.children}
         <InputActionsWrapper>
             <InputButtonWrapper
-                onClick={() => {}}>
-                <Icon icon={iconCheckmark} fill={props.theme.colors.green[60]} height='12px' width='12px'
-                      onClick={props.onValidate}/>
+                onClick={() => props.onValidate(props.inputValue)}>
+                <Icon icon={iconCheckmark}
+                      fill={props.theme.colors.green[60]}
+                      height='12px'
+                      width='12px'/>
             </InputButtonWrapper>
             <InputButtonWrapper
-                onClick={() => {}}>
-                <Icon icon={iconClose} fill={props.theme.colors.red[60]} onClick={props.onCancel}/>
+                onClick={props.onCancel}>
+                <Icon icon={iconClose}
+                      fill={props.theme.colors.red[60]}/>
             </InputButtonWrapper>
         </InputActionsWrapper>
     </InputWrapper>
-
-export const TextInput = compose(withTheme, withInputWrapper)(({ title, value }) =>
-    <StyledTextInput
-        id={kebabCase(title)}
-        hideLabel={true}
-        value={value}
-        labelText=''
-        light/>)
