b55c06b1f1e6c0b4d185f4a78d6f18550f0b70b9
Carvallegro <valentin.carmignac@gmail.com>
2018-11-03 11:49:58 +1100

    refactored character details

--- src/Creator/version4/app/src/store/actions/character-description.js
+++ src/Creator/version4/app/src/store/actions/character-description.js
@@ -1,11 +1,17 @@
+import client from '@client'
+
 export const characterDescriptionTypes = {
     UPDATE_DETAILS: 'CHARACTER_DESCRIPTION_UPDATE_DETAILS'
 }
 
-const updateDescription = details => ({
-    type: characterDescriptionTypes.UPDATE_DETAILS,
-    data: details
-})
+const updateDescription = details => {
+    console.log('DETAILS CALLED')
+    console.log(details)
+    return ({
+        type: characterDescriptionTypes.UPDATE_DETAILS,
+        payload: client.dispatch(details).then(() => details)
+    })
+}
 
 export default {
     updateDescription
